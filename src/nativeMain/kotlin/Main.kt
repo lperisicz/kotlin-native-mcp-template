70b53b5188f81ba77d665f9c58b018864813acb8
lperisicz <lukaperisic97@gmail.com>
2025-08-13 20:25:34 +0200

    added sketch of main TODO roadmap

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,36 +1,7 @@
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import logger.Logger
-import logger.debug
-import logger.error
-import logger.info
-import logger.warn
-
-@Serializable
-private data class Message(
-    val topic: String,
-    val content: String,
-)
-
-private val PrettyPrintJson = Json {
-    prettyPrint = true
-}
-
 public fun main() {
-    val message = Message(
-        topic = "Kotlin/Native",
-        content = "Hello!"
-    )
-    println(PrettyPrintJson.encodeToString(message))
+    // TODO parse arguments (also provide --help)
 
-    Logger.setLogFile("build/someFile.log")
+    // TODO setup logger
 
-    Logger.debug("TAG", "some message for debug")
-    Logger.info("TAG", "some message for info")
-    Logger.warn("TAG", "some message for warn")
-    Logger.error(
-        "TAG",
-        "Some message for error log level",
-        IllegalStateException("Some exception message")
-    )
+    // TODO setup MCP server (transport{stdio, sse(port)})
 }
