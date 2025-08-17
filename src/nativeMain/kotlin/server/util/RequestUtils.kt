0a57fbe4f48376af9000596b78c4150fd727a8ff
BornaMarin <bornamarin3232@gmail.com>
2025-08-17 02:14:14 +0200

    a

--- src/nativeMain/kotlin/server/util/RequestUtils.kt
+++ src/nativeMain/kotlin/server/util/RequestUtils.kt
@@ -4,4 +4,12 @@ import io.ktor.server.request.ApplicationRequest
 
 // TODO define rules for this
 // TODO check if authorisation is provided client side or server side on initial request?
-internal fun ApplicationRequest.extractSessionId(): String? = "123"
+internal fun ApplicationRequest.extractSessionId(): String? =
+    headers["Authorization"]
+        ?.substringAfter("Bearer ")
+        ?: queryParameters
+            .entries()
+            .firstOrNull()
+            ?.value
+            ?.firstOrNull()
+            ?.toString()
